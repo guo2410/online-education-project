bb9d49c37db72ba81d899b473c15ffb80115c870
guo2410 <2410775257@qq.com>
2021-02-02 17:13:42 +0800

    微信扫码登录

---
+++ api/member.js
@@ -0,0 +1,13 @@
+
+import request from '@/utils/request'
+const api_name = '/ucenter/member'
+export default {
+     getInfoByToken(token) {
+       return request({
+         url: `${api_name}/info/${token}`,
+     method: 'post'
+
+    })
+}
+
+}
